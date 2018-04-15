b102bbfa5273593e6cd92d116a3143902ce41986
Hossam Emam <hossamsalahemam@gmail.com>
2018-04-15 20:04:06 +0200

    views

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from .views import home
+from .views import home, detail
 
 
 urlpatterns = [
     url(r'home', home, name="home"),
+    url(r'question/(?P<pk>[0-9]+)/$',detail, name='detail' )
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -7,3 +7,15 @@ def home(request):
     context = {"questions": questions}
 
     return render(request, 'home.html', context)
+
+
+def detail(request, pk):
+    question = Question.objects.get(id=pk)
+    choices = Choice.objects.filter(question=question)
+
+    context = {
+        "question": question,
+        "choices": choices,
+    }
+
+    return render(request, 'details.html', context)
